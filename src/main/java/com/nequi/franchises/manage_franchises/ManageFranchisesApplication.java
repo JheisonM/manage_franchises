6960cf314e9a91753339f5a1cb98d2e380b4df1a
Jheison Martinez <jheison.mb@gmail.com>
2025-01-07 15:13:43 -0500

    initial commit

---
+++ src/main/java/com/nequi/franchises/manage_franchises/ManageFranchisesApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.franchises.manage_franchises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageFranchisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageFranchisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nequi/franchises/manage_franchises/ManageFranchisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.franchises.manage_franchises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageFranchisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
